d081fb52502679960a655a3dcfdbb29fb560c854
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-10-18 22:45:35 +0200

    Initial commit

---
+++ src/Type.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Martijnvdb\TypeVault;
+
+class Type
+{
+    // TODO
+}
---
+++ tests/TypeTest.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Martijnvdb\TypeVault\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class TypeTest extends TestCase
+{
+    // TODO
+}
