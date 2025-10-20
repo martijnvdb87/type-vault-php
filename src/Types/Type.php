036a5c54569621288b3ea1e35f0c18a99ed51e00
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-10-20 08:58:45 +0200

    Use property hooks

--- src/Types/Type.php
+++ src/Types/Type.php
@@ -4,38 +4,38 @@ namespace Martijnvdb\TypeVault\Types;
 
 use Martijnvdb\TypeVault\TypeVaultValidationError;
 
-/**
- * @property mixed $value
- */
 abstract class Type
 {
-    private mixed $state;
     private bool $immutable = false;
     private bool $nullable = false;
+
+    public mixed $value {
+        get => $this->dangerouslyModifyGetValue($this->value);
+
+        set(mixed $value) {
+            $this->assertMutable();
+
+            if ($value === null) {
+                $this->assertNullable();
+
+                $this->value = $this->dangerouslyModifySetValue(null);
+
+                return;
+            }
+
+            $modifiedValue = $this->modifier($value);
+
+            if (!$this->validate($modifiedValue)) {
+                throw new TypeVaultValidationError();
+            }
+
+            $this->value = $this->dangerouslyModifySetValue($value);
+        }
+    }
 
     public function __construct(mixed $value)
     {
         $this->value = $value;
-    }
-
-    public function __get(string $key): mixed
-    {
-        if ($key === 'value') {
-            return $this->getValue();
-        }
-
-        throw new \Exception('Undefined property: ' . $key);
-    }
-
-    public function __set(string $key, mixed $value): void
-    {
-        if ($key === 'value') {
-            $this->setValue($value);
-
-            return;
-        }
-
-        throw new \Exception('Undefined property: ' . $key);
     }
 
     public function isNullable(): bool
@@ -75,32 +75,6 @@ abstract class Type
     protected function modifier(mixed $value): mixed
     {
         return $value;
-    }
-
-    private function getValue(): mixed
-    {
-        return $this->dangerouslyModifyGetValue($this->state);
-    }
-
-    private function setValue(mixed $value): void
-    {
-        $this->assertMutable();
-
-        if ($value === null) {
-            $this->assertNullable();
-
-            $this->state = $this->dangerouslyModifySetValue(null);
-
-            return;
-        }
-
-        $modifiedValue = $this->modifier($value);
-
-        if (!$this->validate($modifiedValue)) {
-            throw new TypeVaultValidationError();
-        }
-
-        $this->state = $this->dangerouslyModifySetValue($value);
     }
 
     abstract protected function validate(mixed $value): bool;
