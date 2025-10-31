1c59bdce6b75de903677342df86d9292a543cf19
Martijn van den Bosch <martijn_van_den_bosch@hotmail.com>
2025-10-31 08:55:10 +0100

    Add validation to ColorHexValuesDTO

--- src/DTOs/ColorHexValuesDTO.php
+++ src/DTOs/ColorHexValuesDTO.php
@@ -2,6 +2,8 @@
 
 namespace Martijnvdb\TypeVault\DTOs;
 
+use Martijnvdb\TypeVault\Errors\TypeVaultValidationError;
+
 final readonly class ColorHexValuesDTO extends DTO
 {
     public function __construct(
@@ -10,6 +12,12 @@ final readonly class ColorHexValuesDTO extends DTO
         public float $blue,
         public float $alpha
     ) {
-        //
+        foreach ([$red, $green, $blue, $alpha] as $value) {
+            if ($value < 0 || $value > 255) {
+                throw new TypeVaultValidationError(
+                    "Value must be between 0 and 255"
+                );
+            }
+        }
     }
 }
